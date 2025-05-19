7147251a1af330fe43d24418300fce93f89d7311
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2025-05-19 15:19:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 var scoreboard = [["Ones",0],["Twos",0],["Threes",0],["Fours",0],["Fives",0],["Sixes",0],["Upper Section Bonus",0],["Three Of a Kind",0],["Four Of a Kind",0],["Full House",0],["Small Straight",0],["Large Straight",0],["Chance",0],["Yahtzee",0]];
 var rounds = 2; // should be 14 when done testing
-function main(){
-    for (let round = 1; round <= rounds; round++){ 
-        playRound(round);
-    //     storeHand();
-    //     roundReset();
-    // }
-    // calcScore();
-    }
-}
 
+/* Class definition for DiceCup */
 class DiceCup{
     constructor(name){
         this.name = name;
         this.hand = [0, 0, 0, 0, 0];
         this.hold = [];
     }
+    
+    /* toggle hold state based on index, so die 1 is at index 0 */
     toggleHold(index){
         let position = this.hold.indexOf(index);
         if (position != -1){
@@ -25,6 +19,11 @@ class DiceCup{
             this.hold.push(index);
         }
     }
+    
+    /* roll held dice 
+     * @param: none;
+     * @return: none;
+     */
     roll(){
         let d = 0;
         for (let die = 0; die < 5; die++){
@@ -32,24 +31,63 @@ class DiceCup{
                 d = Math.floor(Math.random() * 6) + 1;
                 this.hand[die] = d; // this
             }
-
         }
+        console.log(this.hand.toString());
     }
+    
+    /* check if die is being held 
+     * @param: index;
+     * @return: none;
+     */
     isHeld(index){
         return this.hold.includes(index);
     }
-}
 
-function playRound(round) {
+    /* determine dice to reroll with user input 
+     * @param: none;
+     * @return: none;
+     */
+    reroll(){
+    }
+    
+    /* clear all holds 
+     * @param: none;
+     * @return: none;
+     */
+    function reset() {  // To Do
+    }
+    
+} // End of Class Definition
+
+/* Runs the game */
+function main(){
+    const jacob = new DiceCup("jacob"); // Proper syntax = const if we always point to the same object
+    // If multiplayer, need to define a collection for the objects; leave off for now 
+    for (let round = 1; round <= rounds; round++){ 
+        playRound(round);
+    //     storeHand();
+    //     roundReset();
+    // }
+    // calcScore();
+    }
+
+    function playRound(round) {  // Note: defining function inside Main to provide access to objects
     alert("Round " + round + " of " + rounds + ": \n");
-    const DiceCup round = new DiceCup(round);
+    jacob.reset();
+    jacob.roll();
+    
     for (let roll = 1; roll <= 3; roll++){
         rollDice(indices);
         alert("Your hand is: " + hand.toString());
         if (roll < 3 ) indices = selRerolls(roll); // you do not reroll the third roll
     }
     alert(" Where will you score:  " + hand.toString());
+    }
+    
 }
+
+
+
 
 /**  rollDice
  * Rolls one die, stores value
