176c6057e6229815da1c04d32bab313350ec489e
GLUTEN CONSUMER <jacob127871@student-schoolsofwestfield.org>
2025-06-13 15:51:43 +0000

    Finished-ish

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ const categories = ["ones","twos","threes","fours","fives","sixes","upper sectio
 /* Runs the game */
 function main(){
     let rounds = 14; // should be 14 when done testing
-    const cup1 = new DiceCup("cup1"); // Jacob's Cup // Proper syntax = const if we always point to the same object
-    const cup2 = new DiceCup("cup2"); // Bram's Cup
-    const jacob = new ScoreBoard("jacob", cup1);
-    const bram = new ScoreBoard("bram", cup2);
+    let pcount = parseInt(prompt("How many people will be playing?"));
+    let players = [];
+    for (let p = 0; p < pcount; p++){
+        players.push(new ScoreBoard(prompt("What's player" + (p + 1) + "'s name?")));
+    }
     // If multiplayer, need to define a collection for the objects; leave off for now 
-    let player = "jacob";
     for (let round = 1; round <= rounds; round++){ 
-        player = "bram";
-        playRound(round, cup1, player);
-        player = "bram";
-        playRound(round, cup2, player);
+        for (let p = 0; p < pcount; p++){
+            playRound(round, players[p]);
+        }
     }
 
-    function playRound(round, cup, board) {  // Note: defining function inside Main to provide access to objects
+    function playRound(round, board) {  // Note: defining function inside Main to provide access to objects
+        let cup = board.cup;
         console.log("Round " + round + " of " + rounds + " \n");
         console.log(" - " + board.getName() + " roll 1: " + cup.roll());
         console.log(" - " + board.getName() + " reroll 1: " + cup.getHolds());
@@ -28,7 +28,7 @@ function main(){
             category = prompt("Invalid category, please try again.");
         }
         // swap below to player soon
-        while (!jacob.scoreHand(category) && category != "yahtzee"){
+        while (!board.scoreHand(category) && category != "yahtzee"){
             category = prompt("This category has already been scored, try another.");
         }
     }
@@ -37,9 +37,8 @@ function main(){
 
 /* Class definition for DiceCup */
 class DiceCup{
-    constructor(cup){
+    constructor(){
         // this.name = this.getName.bind(this);
-        this.cup = cup;
         this.hand = [0, 0, 0, 0, 0];
         this.hold = [];
     }
@@ -48,7 +47,7 @@ class DiceCup{
      * @return: hand array;
      */
     getHand(){
-        return [5,3,5,3,5]; // UNCOMMENT ONLY FOR TESTING PURPOSES, USED TO DEBUG THREE OF A KIND AND FOUR OF A KIND (5,5,5,5,5 WAS USED FOR YAHTZEE)
+        //return [5,3,5,3,5]; // UNCOMMENT ONLY FOR TESTING PURPOSES, USED TO DEBUG THREE OF A KIND AND FOUR OF A KIND (5,5,5,5,5 WAS USED FOR YAHTZEE)
         return this.hand;
     }
     
@@ -128,9 +127,9 @@ class DiceCup{
 
 /* Class Definition for ScoreBoard */
 class ScoreBoard{
-    constructor(name, cup){
+    constructor(name){
         this.name = name;
-        this.cup = cup;
+        this.cup = new DiceCup();
         this.getHand = this.getHand.bind(this);
         this.board = [];
     }
@@ -149,7 +148,6 @@ class ScoreBoard{
         if (hand.includes("1,2,3,4")) return true;
         else if (hand.includes("2,3,4,5")) return true;
         else if (hand.includes("3,4,5,6")) return true;
-        console.log("hand " + hand);
         return false;
     }
     valLgStraight(){
@@ -198,7 +196,6 @@ class ScoreBoard{
                         if (hand[i] == die) matchCnt++;
                     }
                 }
-                console.log("MatchCnt " + matchCnt);
                 if (count == 3){
                     if (matchCnt <= 3) return true;
                     return false;
@@ -220,7 +217,6 @@ class ScoreBoard{
         else if (categories.indexOf(category) < 6) {
             let counting = categories.indexOf(category) + 1;
             for (let die = 0; die < 5; die++){
-                console.log(" - Counting " + counting);
                 if (hand[die] == counting) {
                     
                     score += counting;
@@ -228,7 +224,6 @@ class ScoreBoard{
             }
         }
         else {
-            console.log(" - Selected category " + category);
             switch (category){
                 case "full house":
                     if (this.valFullHouse()) score = 25;
@@ -279,7 +274,7 @@ class ScoreBoard{
             }
         }
         let newScore = [category, score];
-        console.log("Scoring: " + category + " Score: " + score);
+        // console.log("Scoring: " + category + " Score: " + score);
         this.board.push(newScore);
         let latest = this.board.length-1;
         console.log("Scored: " + this.board[latest].toString());
@@ -293,7 +288,6 @@ class ScoreBoard{
     }
     addUpDice(hand){
         let score = 0;
-        console.log("addUpDice():");
         for (let die = 0; die < 5; die++){
             // console.log(" - addUpDice() for loop");
             // console.log(" - add Up Dice " + die + " - " + hand[die]);
